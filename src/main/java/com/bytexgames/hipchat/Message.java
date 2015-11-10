fa11b412468ce9e914e2910f245f000457d95cf5
Ruslan Balkin <baron@baron.su>
2015-11-10 17:05:51 +0300

    Serialization bugfixes

--- src/main/java/com/bytexgames/hipchat/Message.java
+++ src/main/java/com/bytexgames/hipchat/Message.java
@@ -1,6 +1,7 @@
 package com.bytexgames.hipchat;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
@@ -11,21 +12,15 @@ import com.fasterxml.jackson.annotation.JsonProperty;
  * @author Ruslan Balkin <a href="mailto:baron@baron.su">baron@baron.su</a>
  * @version 1.0.0.0
  */
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public abstract class Message {
-	@JsonProperty("message")
 	private String message;
 
-	@JsonProperty("message_format")
 	private MessageFormat messageFormat;
 
-	@JsonProperty("color")
 	private BackgroundColor color;
 
-	@JsonIgnore
 	private Boolean notify = false;
-
-	@JsonProperty("format")
-	private String format;
 
 	public String getMessage() {
 		return message;
@@ -35,42 +30,39 @@ public abstract class Message {
 		this.message = message;
 	}
 
+	@JsonIgnore
 	public MessageFormat getMessageFormat() {
 		return messageFormat;
 	}
 
-	public void setMessageFormat(MessageFormat messageFormat) {
-		this.messageFormat = messageFormat;
-	}
-
+	@JsonIgnore
 	public BackgroundColor getColor() {
 		return color;
+	}
+
+	public Boolean isNotify() {
+		return notify;
+	}
+
+	@JsonProperty("color")
+	public String getColorInHipchatFormat() {
+		return color == null ? null : color.toString();
+	}
+
+	@JsonProperty("message_format")
+	public String getMessageFormatInHipchatFormat() {
+		return messageFormat.toString();
 	}
 
 	public void setColor(BackgroundColor color) {
 		this.color = color;
 	}
 
-	@JsonIgnore
-	public Boolean isNotify() {
-		return notify;
-	}
-
-	@JsonProperty("notify")
-	public Integer getNotifyInHipchatFormat() {
-		return notify ? 1 : 0;
+	public void setMessageFormat(MessageFormat messageFormat) {
+		this.messageFormat = messageFormat;
 	}
 
 	public void setNotify(Boolean notify) {
 		this.notify = notify;
 	}
-
-	public String getFormat() {
-		return format;
-	}
-
-	public void setFormat(String format) {
-		this.format = format;
-	}
-
 }
